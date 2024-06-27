ed241ee4e940ff622ff2ca383804592231f5dbe0
Luca <luca@lambia.it>
2024-06-27 14:52:41 +0200

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Laravel</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <h1>Hello Laravel World!</h1>
+    <h2>Questo scaffold è stato scritto da Luca Lambiase</h2>
+    
+    <p>Ringraziamenti speciali:</p>
+    <ul>
+        <li>Alexander Fleming</li>
+        <li>Carl Sagan</li>
+        <li>Neil de Grasse Tyson</li>
+        <li>Patrick Stewart</li>
+        <li>John Carmack</li>
+        <li>Julian Assange</li>
+        <li>Sami Antero Järvi</li>
+        <li>Brewster Kahle</li>
+    </ul>
+        
+</body>
+
+</html>
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+//Per la rotta / carica il file "welcome"
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//Per la rotta /credits carica il file "about"
+Route::get('/credits', function () {
+    return view('about');
+});
+
+//Per la rotta /test carica sempre il file "about"
+Route::get('/test', function () {
+    return view('about');
+});
